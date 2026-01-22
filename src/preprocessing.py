604e06a96a14b0be0a611b6f5567b9640c4738e1
jiai27 <carbonell.joaquin27@gmail.com>
2026-01-21 18:43:18 -0700

    proper changes
    checking to see if merging bug is fixed

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,22 +11,7 @@ import cv2 as cv
 import numpy as np
 print(cv.__version__)
 
-<<<<<<< HEAD
-import kagglehub as kg
-# Download latest version
-path = kg.dataset_download("nibinv23/iam-handwriting-word-database")
-print("Path to dataset files:", path)
 
-
-#key = KGAT_2bebedb4bff0533779c78bf4e6c3d1f5
-kg.login()
-
-
-
-#grayscaling, binarize, grid cells (known dimensions, work on unknown later), crop character images, resize to a fixed resolution
-#refer to opencv repo for above
-
-=======
 #-------------------
 #later development:
 
@@ -42,4 +27,3 @@ def gray_binarize(image):
 
 def segment_image(image):
     pass
->>>>>>> 0a3e2bc125f1516fd9c56fd8a756028caf53a1f1
