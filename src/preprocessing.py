0a3e2bc125f1516fd9c56fd8a756028caf53a1f1
joaq <carbonell.joaquin27@gmail.com>
2026-01-21 18:32:51 -0700

    mild adjustments before tackling opencv practice
    note: data is all ignored but the dataset is locally downloaded on MAC, so just download it on PC as well

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,28 @@
 # Author: jiai27 (Joaquin Carbonell)
 # Description:   used to pre-process images using openCV
 
+#grayscaling, binarize, grid cells (known dimensions, work on unknown later), crop character images, resize to a fixed resolution
+#refer to opencv repo
+#intended input: an image with handwriting on it, real or online handwriting
+#developing input: already segmented words, needs to be segmented to letters
+
+#---dependencies---
 import cv2 as cv
 import numpy as np
 print(cv.__version__)
 
-#grayscaling, binarize, grid cells (known dimensions, work on unknown later), crop character images, resize to a fixed resolution
-#refer to opencv repo
+#-------------------
+#later development:
+
+#word segmentation (sentence -> words), should return a list?
+
+example_words = []
+
+
+#grayscale & binarize
+def gray_binarize(image):
+    pass
+
+
+def segment_image(image):
+    pass
